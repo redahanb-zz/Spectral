fe9da40cc3a48621a9f4853dbcece4b03998b058
Ben Redahan <redahanb@gmail.com>
2015-07-23 14:46:59 +0100

    Added Dynamic UI building
    - Added and updated scripts for building HUD elements (healthbar and
    inventory) at runtime
    - Element positioning is slightly off: it’s perfectly regular but the
    Unity editor is leaving a few pixels gap between elements for some
    reason
    - Next up: functionality for interacting with the player’s health and
    inventory components (NB: player’s or game manager’s components?)

---
+++ Assets/Scripts/UI/HUD/HUD_Healthbar.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD_Healthbar : MonoBehaviour {
+
+	GameObject[] healthbarIcons;
+	public int panelSize;
+	
+	// Use this for initialization
+	void Start () {
+		buildHealthbarUI (5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void buildHealthbarUI(int size) {
+		// Take size from player inventory, instantiate UI box for each slot, plus bookend graphics
+		healthbarIcons = new GameObject[size];
+		Vector3 panelPosition;
+		
+		// draw shoulder buttons
+		GameObject L_bookend = Instantiate (Resources.Load("UI/HUD/Health/HUD_Bookend_Health_L"), new Vector3(-1*((size*panelSize)/2) - panelSize/2, -10, 0), Quaternion.identity) as GameObject;
+		L_bookend.transform.SetParent(gameObject.transform, false);
+		L_bookend.GetComponent<RectTransform>().anchoredPosition = new Vector2(-1*((size*panelSize)/2) - panelSize/2, -10) ;
+		
+		GameObject R_bookend = Instantiate (Resources.Load("UI/HUD/Health/HUD_Bookend_Health_R"), new Vector3(1*((size*panelSize)/2) + panelSize/2, -10, 0), Quaternion.identity) as GameObject;
+		R_bookend.transform.SetParent(gameObject.transform, false);
+		R_bookend.GetComponent<RectTransform>().anchoredPosition = new Vector2(1*((size*panelSize)/2) + panelSize/2, -10) ;
+		
+		for(int i = 0; i < size ; i++){
+			// position their rect-transforms according to their index and the size of the array
+			panelPosition = new Vector3( -1*((size*panelSize)/2) + panelSize/2 + i*panelSize, -10, 0);
+			healthbarIcons[i] = Instantiate(Resources.Load("UI/HUD/Health/HUD_Panel_Health"), panelPosition, Quaternion.identity) as GameObject;
+			
+			// set the parent and keep the position relative to the parent
+			healthbarIcons[i].transform.SetParent(gameObject.transform, false);
+			healthbarIcons[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1*((size*panelSize)/2) + panelSize/2 + i*panelSize, -10);
+			
+			// name them as an index for the UI array
+			healthbarIcons[i].gameObject.name = "Cell " + i.ToString();
+		}
+		
+	}
+}
---
+++ Assets/Scripts/UI/HUD/HUD_Inventory.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD_Inventory : MonoBehaviour {
+
+	GameObject[] inventoryIcons;
+	public int panelSize;
+
+	// Use this for initialization
+	void Start () {
+		buildInventoryUI (5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void buildInventoryUI(int size) {
+		// Take size from player inventory, instantiate UI box for each slot, plus bookend graphics
+		inventoryIcons = new GameObject[size];
+		Vector3 panelPosition;
+
+		// draw shoulder buttons
+		GameObject L_bookend = Instantiate (Resources.Load("UI/HUD/Inventory/HUD_Bookend_Inv_L"), new Vector3(-1*((size*panelSize)/2) - panelSize/2, 20, 0), Quaternion.identity) as GameObject;
+		L_bookend.transform.SetParent(gameObject.transform, false);
+		L_bookend.GetComponent<RectTransform>().anchoredPosition = new Vector2(-1*((size*panelSize)/2) - panelSize/2, 20) ;
+		
+		GameObject R_bookend = Instantiate (Resources.Load("UI/HUD/Inventory/HUD_Bookend_Inv_R"), new Vector3(1*((size*panelSize)/2) + panelSize/2, 20, 0), Quaternion.identity) as GameObject;
+		R_bookend.transform.SetParent(gameObject.transform, false);
+		R_bookend.GetComponent<RectTransform>().anchoredPosition = new Vector2(1*((size*panelSize)/2) + panelSize/2, 20) ;
+		
+		for(int i = 0; i < size ; i++){
+			// position their rect-transforms according to their index and the size of the array
+			panelPosition = new Vector3( -1*((size*panelSize)/2) + panelSize/2 + i*panelSize, 20, 0);
+			inventoryIcons[i] = Instantiate(Resources.Load("UI/HUD/Inventory/HUD_Panel_Inv"), panelPosition, Quaternion.identity) as GameObject;
+			
+			// set the parent and keep the position relative to the parent
+			inventoryIcons[i].transform.SetParent(gameObject.transform, false);
+			inventoryIcons[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1*((size*panelSize)/2) + panelSize/2 + i*panelSize, 20);
+			
+			// name them as an index for the UI array
+			inventoryIcons[i].gameObject.name = "Cell " + i.ToString();
+		}
+		
+	}	
+}

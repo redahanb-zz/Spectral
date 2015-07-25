c96e244a6144a4b013c6af1f0d508b0ed255f724
Ben Redahan <redahanb@gmail.com>
2015-07-25 21:34:44 +0100

    Added Testing for Touch Controls of Inventory
    - Created a test ‘Touch Inventory’ script to handle drag and drop
    functionality of the player inventory
    - Basic drag and drop feature is functioning, needs Player Inventory
    script to proceed further
    - Touch controls need refinement/precision: what happened when there is
    more than one touch at once? How to cancel the dropping of the time?
    Timer to separate a drop from choosing the colour?

--- Assets/Test Scenes/Testing New UI/TouchInventory.cs
+++ Assets/Test Scenes/Testing New UI/TouchInventory.cs
@@ -1,21 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+
+using UnityEngine.UI;
 
 public class TouchInventory : MonoBehaviour {
+	
+	EventSystem eventSystem;
 
+	Text text;
 
+	//Vector2 startLocation;
+	//Vector2 endLocation;
+	GameObject floatImage;
 
 	// Use this for initialization
 	void Start () {
-	
+		eventSystem = GameObject.Find ("EventSystem").GetComponent<EventSystem>();
+
+		text = GameObject.Find ("ReportText").GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		print (eventSystem.currentSelectedGameObject);
 
 		foreach(Touch touch in Input.touches){
-			//if(){}
+
+			if(touch.phase == TouchPhase.Began){
+				text.text = "Finger ID: " + touch.fingerId.ToString();
+				floatImage = Instantiate(eventSystem.currentSelectedGameObject.transform.GetChild(0).gameObject, touch.position, Quaternion.identity) as GameObject;
+				floatImage.transform.SetParent(GameObject.Find("Canvas").transform);
+				//startLocation = touch.position;
+
+			} else if (touch.phase == TouchPhase.Moved){
+				floatImage.transform.position = touch.position;
+			} else if(touch.phase == TouchPhase.Ended){
+				//endLocation = touch.position;
+				floatImage.SetActive(false);
+				Destroy(floatImage);
+			}
 		}
 	}
 }

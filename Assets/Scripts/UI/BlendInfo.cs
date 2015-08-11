7358d49e7a562b044474e8d77a1948261b433e47
Ben Redahan <redahanb@gmail.com>
2015-08-11 15:36:15 +0100

    Mixed Updates
    - Fixed bug with object pickup animations
    - Added mouse drag and drop to inventory controls
    - Added pause button to main canvas
    - Added new animations for pickup buttons
    - Added button for bendable object interaction (Requires a custom icon)
    - Updated robot guard prefab
    
    TODO:
    - Guard projectiles
    - Integrate shooting with player health
    - Display item value in inventory HUD
    - Complete pause screen
    - Update blend icon and pickup icon
    - Update nav mesh waypoint for player

---
+++ Assets/Scripts/UI/BlendInfo.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlendInfo : MonoBehaviour {
+
+	GameObject button;
+	GameObject canvasObject;
+	
+	InventoryItem inventoryItem;
+	
+	// Use this for initialization
+	void Start () {
+		canvasObject = GameObject.Find ("Canvas");
+		inventoryItem = GetComponent<InventoryItem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			if(!button){
+				button = Instantiate(Resources.Load("UI/Worldspace Buttons/Blend Info"), canvasObject.transform.position, Quaternion.identity) as GameObject;
+				button.transform.SetParent(canvasObject.transform);
+				button.GetComponent<BlendInfoButton>().setTarget(gameObject);
+				//button.GetComponent<Button>().onClick.AddListener( button.GetComponent<ItemInfoButton>().target.GetComponent<InventoryItem>().addItemToInventory );
+				//button.GetComponent<Button>().onClick.AddListener( button.GetComponent<ItemInfoButton>().target.GetComponent<InventoryItem>().pickupAnim );
+				//inventoryItem.identifyButton(button);
+			} else if(button.GetComponent<Button>().interactable == false){
+				button.GetComponent<Button>().interactable = true;
+			}
+		}
+		
+		// reactivate in case the object has been put back onto the ground (button deactivates then)
+		//button.SetActive (true);
+	}
+	
+	void OnTriggerExit(Collider col){
+		if (col.tag == "Player") {
+			button.GetComponent<Button>().interactable = false;
+		}
+	}
+}

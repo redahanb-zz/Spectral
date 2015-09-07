30dd15c1801f81ffcd62687437af10b98681a6fb
Ben Redahan <redahanb@gmail.com>
2015-09-07 08:59:33 +0100

    Added Guard Self Destruct Behaviour
    - Created self destruct behaviour for guards where they explode into
    particles
    - Started player polygon model

---
+++ Assets/GuardSelfDestruct.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GuardSelfDestruct : MonoBehaviour {
+
+	public GameObject[] bodyparts = new GameObject[29];
+	//List<GameObject> bodyparts = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("selfDestruct", 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void selfDestruct(){
+		foreach(GameObject part in bodyparts){
+			part.transform.parent = null;
+			part.AddComponent<Rigidbody>();
+			Vector3 impulse = new Vector3(Random.Range(-0.05f,0.05f),Random.Range(0.1f,0.3f),Random.Range(-0.05f,0.05f));
+			part.GetComponent<Rigidbody>().AddForce(impulse);
+			part.AddComponent<CapsuleCollider>();
+			Destroy (part, Random.Range(0.5f,1.5f));
+		}
+		Destroy (this.gameObject);
+		Destroy (this.transform.parent.gameObject);
+	}
+}

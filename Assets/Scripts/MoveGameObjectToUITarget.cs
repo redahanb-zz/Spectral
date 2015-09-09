66028aae7543de7d46a9442d5547476f201dcad4
Conor Hughes <Daisy2014>
2015-09-10 00:56:39 +0100

    Created level select and home base scenes

---
+++ Assets/Scripts/MoveGameObjectToUITarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveGameObjectToUITarget : MonoBehaviour {
+
+	public Vector3 objectPosition, targetObjectPosition, uiElementPosition;
+
+	public GameObject targetUIElement;
+
+	RectTransform rtransform;
+
+	// Use this for initialization
+	void Start () {
+		rtransform = targetUIElement.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		uiElementPosition = rtransform.position;
+		transform.position = Camera.main.ScreenToWorldPoint(uiElementPosition);
+	}
+}

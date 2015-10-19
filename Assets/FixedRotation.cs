7f276e3e1ceb93b3618dc3c99ef3a6d6eab4c574
Conor Hughes <Daisy2014>
2015-10-19 09:52:11 +0100

    Motor Tree intro, loading for home base
    -Created animated Motor Tree intro.
    -Created initial transition to the Restore point level.
    -Changed loading to asynchronous loading so the Restore point should
    transition faster.
    -Fixed the missing mesh issue in the Restore Point level.

---
+++ Assets/FixedRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotation : MonoBehaviour {
+
+	Quaternion rotation;
+	Vector3 postion;
+	void Awake()
+	{
+		rotation = transform.rotation;
+		postion  = transform.position;
+	}
+	void LateUpdate()
+	{
+		transform.rotation = rotation;
+		transform.position = postion;
+	}
+}
